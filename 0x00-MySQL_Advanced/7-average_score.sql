f5459c1685bcc6f4d18276b7a7a445f5a95af9cd
Fiona-Nyadero <fionanyadero@gmail.com>
2024-04-25 22:06:33 +0000

    Task 7 corr

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,19 +1,15 @@
 -- creates a stored procedure that computes
 -- & stores the average student score
 
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
-
-    SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = user_id;
-    SELECT COUNT(*) INTO total_projects FROM corrections WHERE user_id = user_id;
-
-    IF total_projects > 0 THEN
-        UPDATE users SET average_score = total_score / total_projects WHERE id = user_id;
-    END IF;
+	UPDATE users
+	SET aver_sc = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
 END //
 
 DELIMITER ;
