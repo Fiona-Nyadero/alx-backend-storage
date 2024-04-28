140abb9d51fc3c2914fbae59bdd5b971d4681d8b
Fiona-Nyadero <fionanyadero@gmail.com>
2024-04-28 01:28:30 +0000

    Task 11

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- SQL to create views
+
+CREATE VIEW need_meeting
+AS SELECT name
+FROM students
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
