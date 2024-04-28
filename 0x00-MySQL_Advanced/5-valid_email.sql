4ea0dedbaa984347081d5a899488753e8659dbab
Fiona-Nyadero <fionanyadero@gmail.com>
2024-04-28 01:14:46 +0000

    Tasks 10corr

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,10 +1,11 @@
 --trigger resets the attribute valid_emaili
 -- when email is changed
 DELIMITER //
-CREATE TRIGGER reset_email_trigger
-BEFORE UPDATE ON users
+CREATE TRIGGER reset_email_trigger BEFORE UPDATE ON users
 FOR EACH ROW
+BEGIN
     IF NEW.email <> OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
+END;//
 DELIMITER ;
