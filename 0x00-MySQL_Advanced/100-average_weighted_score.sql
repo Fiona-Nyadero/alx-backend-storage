39675a38698217cbd8662cb24a6c48f43b627f54
Fiona-Nyadero <fionanyadero@gmail.com>
2024-04-28 02:34:24 +0000

    Task 12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -5,7 +5,7 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
     UPDATE users
-    SET average_weight_score = (
+    SET average_score = (
         SELECT IF(SUM(score * weight) > 0, SUM(score * weight) / SUM(weight), 0)
         FROM corrections
         JOIN projects ON corrections.project_id = projects.id
