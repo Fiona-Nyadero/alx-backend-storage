9d5d797f6c88a095045d92f4dab69db737256d68
Fiona-Nyadero <fionanyadero@gmail.com>
2024-04-30 19:59:43 -0700

    Task12 corr

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,26 +3,29 @@
 
 import pymongo
 
+def nginx_logs_stats():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["logs"]
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["logs"]
+    nginx_col = db["nginx"]
 
-nginx_col = db["nginx"]
+    all_logs = nginx_col.count_documents({})
+    print(f"{all_logs} logs")
 
-all_logs = nginx_col.count_documents({})
-print(f"{all_logs} logs")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {}
+    for method in methods:
+        cnt = nginx_col.count_documents({"method": method})
+        method_counts[method] = cnt
+        print("Methods:")
+    for method, cnt in method_counts.items():
+        print(f"    method {method}: {cnt}")
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {}
-for method in methods:
-    cnt = nginx_col.count_documents({"method": method})
-    method_counts[method] = cnt
-print("Methods:")
-for method, cnt in method_counts.items():
-    print(f"    method {method}: {cnt}")
-
-spec_method = "GET"
-spec_path = "/status"
-spec_count = nginx_col.count_documents(
+    spec_method = "GET"
+    spec_path = "/status"
+    spec_count = nginx_col.count_documents(
         {"method": spec_method, "path": spec_path})
-print(f"{spec_count} status check")
+    print(f"{spec_count} status check")
+
+if __name__ == "__main__":
+    nginx_logs_stats()
