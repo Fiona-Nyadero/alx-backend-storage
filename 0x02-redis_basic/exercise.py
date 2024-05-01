96a9e77f0ad8edcc25dda510c851e62f261bbd09
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-01 19:16:34 +0000

    Task2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,20 @@
 import redis
 import uuid
 from typing import Union, Callable, Optional
+import functools
+
+
+def count_calls(method: Callable) -> Callable:
+    '''Counts the no. of times a method is called'''
+
+
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''wrapper function for the count_calls decorator'''
+        schulssel = method.__qualname__
+        self.redis_db.incr(schulssel)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -10,20 +24,20 @@ class Cache:
 
     def __init__(self):
         '''Stores an instance of Redis Client'''
-        self._redis = redis.Redis()
-        self._redis.flushdb()
+        self.redis_db = redis.Redis()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Stores data in Redis & generates a random key'''
         schulssel = str(uuid.uuid4())
-        self._redis.set(schulssel, data)
+        self.redis_db.set(schulssel, data)
         return schulssel
 
     def get(
         self, key: str, fn: Optional[Callable] = None
     ) -> Union[str, bytes, int, float]:
         '''Retrieves data from Redis'''
-        info = self._redis.get(key)
+        info = self.redis_db.get(key)
         if info is None:
             return None
         if fn:
